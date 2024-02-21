4d2acfd1171920c6cb4a1f512c0df233bf560ea7
Andrew <86363077+catc24@users.noreply.github.com>
2024-02-20 21:42:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,10 @@ fetch('https://api.weather.gov/gridpoints/APX/81,45/forecast/hourly?units=us')
     // Check if <dew> tag exists, if so update its content
     let dewElement = document.querySelector('dew');
     if(dewElement) {
-        dewElement.textContent =  ${dewpointF}°F`;
+        dewElement.textContent = `${dewpointF}°F`;
     }
 })
 .catch(error => console.error('Error:', error));
-
 
 
 
