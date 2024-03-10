af21e64a565ebcb9dea18005a9a2fbc12bdc01c4
Andrew <86363077+catc24@users.noreply.github.com>
2024-03-09 20:05:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -153,21 +153,7 @@ fetch('https://api.weather.gov/gridpoints/APX/81,45/forecast/hourly?units=us')
 
 
 
-<!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://andrewtheweatherguy.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/andrewtheweatherguy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
+
 
 
 
