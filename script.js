16f966cac74f3d8c3d11ddfe35a4884c7872a115
Andrew <86363077+catc24@users.noreply.github.com>
2024-04-28 10:44:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -112,8 +112,6 @@ fetch("https://api.weather.gov/gridpoints/APX/81,45/forecast/hourly?units=us")
 
 // List of images
 var images = [
-  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/latest.jpg",
-  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/Sandwich/2500x1500.jpg",
   "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/GOES16-CONUS-GEOCOLOR-625x375.gif",
   // Add more images as needed
 ];
